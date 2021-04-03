1a8eb5ffd056cd48413abbc127edc4167ec88c9c
BrockBaron <Nslekkas@gmail.com>
2021-04-03 19:27:22 +0100

    controller update

--- gym_app/controllers/class_controller.py
+++ gym_app/controllers/class_controller.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request,, redirect
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.exclass import Class
 
 import repositories.class_repository as class_repository
 
+class_blueprint = Blueprint("classes", __name__)
+
+@class_blueprint.route('/classes')
+def classes():
+    classes = class_repository.select_all()
+    return render_template('classess/index.html', classes = classes)
 
 #CREATE NEW CLASS
 
