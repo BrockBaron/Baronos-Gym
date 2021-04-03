50239fbfbbe2dcff28aad04b741aa9f307e6dc4b
BrockBaron <Nslekkas@gmail.com>
2021-04-03 20:55:02 +0100

    exclass controller update

--- gym_app/controllers/exclass_controller.py
+++ gym_app/controllers/exclass_controller.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, request, redirect
-from flask import Blueprint
+from controllers.member_controller import member
+from flask import Flask, render_template, request, redirect, Blueprint
+
 from models.exclass import Class
 
 import repositories.exclass_repository as exclass_repository
+import repositories.member_repository as member_repository
 
-class_blueprint = Blueprint("classes", __name__)
+exclass_blueprint = Blueprint("classes", __name__)
 
-@class_blueprint.route('/classes')
+@exclass_blueprint.route('/classes')
 def classes():
     classes = exclass_repository.select_all()
     return render_template('classess/index.html', classes = classes)
+
+# SHOW CLASSES
+@exclass_blueprint.route('/classes/<id>')
+def show(id):
+    exclass = exclass_repository.select(id)
+    members = member_repository.get_by_class(exclass)
+    return render_template('classes//show.html', exclass = exclass, members = members)
 
 #CREATE NEW CLASS
 
