946614be2050d809a2426e944c2f7a7bcabbdcfe
BrockBaron <Nslekkas@gmail.com>
2021-04-03 12:33:08 +0100

    lunchtime

---
+++ gym_app/controllers/class_controller.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request,, redirect
+from flask import Blueprint
+from models.exclass import Class
+
+import repositories.class_repository as class_repository
+
+
+#CREATE NEW CLASS
+
+#UPDATED CLASS
+
+#DELETE CLASS
