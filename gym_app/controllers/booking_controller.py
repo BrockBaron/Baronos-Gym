d1bebbeec26248e0a88a10ab7bd6c0acdd2b1e38
BrockBaron <Nslekkas@gmail.com>
2021-04-03 19:43:23 +0100

    controller update

--- gym_app/controllers/booking_controller.py
+++ gym_app/controllers/booking_controller.py
@@ -1,6 +1,20 @@
+from controllers.member_controller import member
+from controllers.exclass_controller import exclass
+from flask import Flask, render_template, request, redirect, Blueprint
 
+from models.booking import Booking
 
+import repositories.class_repository as class_repository
+import repositories.member_repository as member_repository
+import repositories.booking_repository as booking_repository
 
+bookings_blueprint = Blueprint("bookins", __name__)
+
+@bookings_blueprint.rout("/classes")
+def bookins():
+    bookings = booking_repository.select_all()
+    return render_template('bookins/index.html', booking=bookings)
+    
 
 #ADD MEMEBER TO CLASS 
 
