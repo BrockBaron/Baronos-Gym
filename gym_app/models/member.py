dc4c4b8bdad6f8962a0e1456e1e4b8abde3afc37
BrockBaron <Nslekkas@gmail.com>
2021-04-03 12:18:42 +0100

    members and spelling errors

--- gym_app/models/member.py
+++ gym_app/models/member.py
@@ -1,7 +1,8 @@
 class Member:
-    def __init__(self, first_name, second_name, age, id=None):
+    def __init__(self, first_name, second_name, age, sex, id=None):
         self.first_name = first_name
         self.second_name = second_name
         self.age = age
+        self.sex = sex
         self.id = id
     
