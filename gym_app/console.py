3cb300d6dc82e96b356cb6a477f165a7b8d8a8ec
BrockBaron <Nslekkas@gmail.com>
2021-04-03 20:15:40 +0100

    controller errors update

--- gym_app/console.py
+++ gym_app/console.py
@@ -6,11 +6,11 @@ from models.exclass import Class
 
 import repositories.booking_repository as booking_repository
 import repositories.member_repository as member_repository
-import repositories.class_repository as class_repository
+import repositories.exclass_repository as exclass_repository
 
 booking_repository.delete_all()
 member_repository.delete_all()
-class_repository.delete_all()
+exclass_repository.delete_all()
 
 
 
--- gym_app/controllers/booking_controller.py
+++ gym_app/controllers/booking_controller.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, Blueprint
 
 from models.booking import Booking
 
-import repositories.class_repository as class_repository
+import repositories.exclass_repository as exclass_repository
 import repositories.member_repository as member_repository
 import repositories.booking_repository as booking_repository
 
