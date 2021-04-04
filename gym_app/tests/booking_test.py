0296fed2e5aab75ceba1c1dfed8d2b0394e8fd31
BrockBaron <Nslekkas@gmail.com>
2021-04-04 20:56:34 +0100

    test set up

--- gym_app/tests/booking_test.py
+++ gym_app/tests/booking_test.py
@@ -8,12 +8,12 @@ class TestBooking(unittest.TestCase):
 
 
 #1 booking has name 
-@unittest.skip("Delete this line to run the test")      
-def test_booking_has_members(self):
-    self.assertEqual("Max Power", self.booking.name)
+    @unittest.skip("Delete this line to run the test")      
+    def test_booking_has_members(self):
+        self.assertEqual("Max Power", self.booking.name)
         
 #2 booking has exclasses
-@unittest.skip("Delete this line to run the test")
-def test_booking_has_exclasses(self):
-    self.assertEqual("Big Lift", self.booking.exclass)
+    @unittest.skip("Delete this line to run the test")
+    def test_booking_has_exclasses(self):
+        self.assertEqual("Big Lift", self.booking.exclass)
     
--- gym_app/tests/member_test.py
+++ gym_app/tests/member_test.py
@@ -4,3 +4,24 @@ from models.member import Member
 class TestMember(unittest.TestCase):
     
     def setUp(self):
+        self.member = Member('Max', 'Power', 40, 'Male')
+        
+    #1 member has name first
+    @unittest.skip("Delete this line to run the test")
+    def test_member_has_first_name(self):
+        self.assertEqual("Max", self.memebr.first_name)
+    
+    #2 member has name second name
+    @unittest.skip("Delete this line to run the test")
+    def test_member_has_second_name(self):
+        self.assertEqual("Power", self.memebr.second_name)
+        
+    #3 member has name age
+    @unittest.skip("Delete this line to run the test")
+    def test_member_has_age(self):
+        self.assertEqual(40, self.memebr.age)
+    
+    #4 member has name second name
+    @unittest.skip("Delete this line to run the test")
+    def test_member_has_sex(self):
+        self.assertEqual("Male", self.memebr.sex)
