ad9b2eb0b498f24d8884c0d38cc3ab768a9122ed
BrockBaron <Nslekkas@gmail.com>
2021-04-04 23:29:14 +0100

    actually bedime :( - conseole update

--- gym_app/console.py
+++ gym_app/console.py
@@ -1,8 +1,9 @@
 import pdb
+from re import M
 
 from models.booking import Booking
 from models.member import Member
-from models.exclass import Class
+from models.exclass import Exclass
 
 import repositories.booking_repository as booking_repository
 import repositories.member_repository as member_repository
@@ -14,11 +15,94 @@ exclass_repository.delete_all()
 
 
 
+member1 = Member('Max', 'Power', 40, 'Male')
+member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
+# member1 = Member('Max', 'Power', 40, 'Male')
+# member_repository.save(member1)
 
+exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
+# exclass1 = Exclass('Keen', 'Spinning', 90, 15)
+# exclass_repository.save(exclass1)
 
-
-
-
+booking1 = Booking(member1, exclass1)
+booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
+# booking1 = Booking(member1, exclass1)
+# booking_repository.save(booking1)
 
 
 
