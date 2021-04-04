28b8d3bed09b8f0b938f7fa3394b662ff3b8ded8
BrockBaron <Nslekkas@gmail.com>
2021-04-04 20:09:20 +0100

    booking repo add booking

--- gym_app/repositories/booking_repository.py
+++ gym_app/repositories/booking_repository.py
@@ -5,12 +5,21 @@ import repositories.exclass_repository as exclass_repository
 import repositories.member_repository as member_repository
 
 # save new bookings, generate uniques booking number / id
+def save(booking):
+    sql = "INSER INTO bookings (member_id, exclass_id) VALUES (%s, %s, %s) RETURNING id"
+    values = [booking.member.id, booking.exclass.id]
+    results = run_sql(sql, values)
+    booking.id = results[0]['id']
+    return booking
+
 
 # select all bookings
 
 # delete all bookings
 
 # delete individual booking by id
+
+# ??show all members booked on specific class?? take/replace code from member repo 
 
 
 
