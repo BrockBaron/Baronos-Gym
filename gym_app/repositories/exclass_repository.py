bea7a3aa8142853a81bf4b4a14e5cd17c601da17
BrockBaron <Nslekkas@gmail.com>
2021-04-04 19:47:09 +0100

    exclass repo save new class

--- gym_app/repositories/exclass_repository.py
+++ gym_app/repositories/exclass_repository.py
@@ -3,7 +3,13 @@ from models.exclass import Class
 from models.member import Member
 
 # save new exclass
-
+def save(exclass):
+    sql = "INSERT INTO exclass(name, activity_type, duration, capacity) VALUES (%s, %s, %s, %s) RETURNING id"
+    values = [exclass.name, exclass.activity_type, exclass.duration, exclass.capacity]
+    results = run_sql(sql, values)
+    exclass.id == results[0]['id']
+    return exclass
+    
 # select all classes
 
 # select specific class by id
