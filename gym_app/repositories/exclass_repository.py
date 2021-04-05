ba17008d6cfd0837434fcfb04055f19feb534778
BrockBaron <Nslekkas@gmail.com>
2021-04-05 11:55:07 +0100

    more bugs & fixes

--- gym_app/repositories/exclass_repository.py
+++ gym_app/repositories/exclass_repository.py
@@ -7,7 +7,7 @@ def save(exclass):
     sql = "INSERT INTO exclasses(name, activity_type, duration, capacity) VALUES (%s, %s, %s, %s) RETURNING id"
     values = [exclass.name, exclass.activity_type, exclass.duration, exclass.capacity]
     results = run_sql(sql, values)
-    exclass.id == results[0]['id']
+    exclass.id = results[0]['id']
     return exclass
     
 # select all classes
