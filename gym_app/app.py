e4788bfbfd6537ddaa827fd9952d3f6a59dd6534
BrockBaron <Nslekkas@gmail.com>
2021-04-05 11:46:26 +0100

    more bugs

--- gym_app/app.py
+++ gym_app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 from controllers.booking_controller import bookings_blueprint
-from controllers.exclass_controller import exclasses_blueprint
+from controllers.exclass_controller import exclass_blueprint
 from controllers.member_controller import members_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(bookings_blueprint)
-app.register_blueprint(exclasses_blueprint)
+app.register_blueprint(exclass_blueprint)
 app.register_blueprint(members_blueprint)
 
 @app.route('/')
